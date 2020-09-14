9eddd1c2d904f9e6ff9c2d2300e6fe5417fc5a2d
lingfeng <413957294@qq.com>
2020-09-15 00:36:09 +0800

    bug fixed

--- static/js/gitment.js
+++ static/js/gitment.js
@@ -3413,7 +3413,7 @@ var Gitment =
                     }, options);
 
                     this.state.user.isLoggingIn = true;
-                    _utils.http.post('https://geekertoken.com/blog/comments/', {
+                    _utils.http.post('https://geekertoken.com/blog/comment', {
                         code: code,
                         client_id: client_id,
                         client_secret: client_secret
